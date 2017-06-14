59e1b633cc7e35e09465ff36acb4b58542f24185
karim33 <abdulkerim333@gmail.com>
2017-06-13 19:08:23 -0700

    Added some functionality to render encrypted text

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form action="/" method="POST">
+        <label>Rotate by:</label>
+        <input type="text" name="rot" value=0 />
+        <textarea name="text">{0}</textarea>
+        <input type="submit"/>
+    </body>
+</html>
+"""
+
+
+@app.route("/")
+def index():
+    return form.format("")
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot=request.form["rot"]
+    text = request.form["text"]
+    return '<h1>'+ form.format(rotate_string(text, int(rot)))+'</h1>'
+
+app.run()
